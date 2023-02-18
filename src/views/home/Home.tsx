38fe2ff4ff7f74105dc10cdd58a26ecd41f0ec4d
latifakone <konelamine20121999@gmail.com>
2023-02-18 00:53:30 +0000

    new commit

--- src/views/home/Home.tsx
+++ src/views/home/Home.tsx
@@ -3,8 +3,31 @@ import Header from "../../layouts/fronted/header/Header";
 import Banniere from "./baniere/Banniere";
 import image01 from '../../assets/images/image01.jpg'
 import './Home.css'
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
+import BasicCard from "./card/Card";
+import Footer from "../../layouts/fronted/footer/Footer"
 
-export default function Home() {
+export default function Home(props: any) {
+
+    const responsive = {
+        desktop: {
+          breakpoint: { max: 3000, min: 1175 },
+          items: 4,
+          slidesToSlide: 4 // optional, default to 1.
+        },
+        tablet: {
+          breakpoint: { max: 1175, min: 464 },
+          items: 3,
+          slidesToSlide: 3 // optional, default to 1.
+        },
+        mobile: {
+          breakpoint: { max: 464, min: 0 },
+          items: 1,
+          slidesToSlide: 1 // optional, default to 1.
+        }
+    };
+
     return (
         <div>
             <Header/>
@@ -36,9 +59,64 @@ export default function Home() {
                     </div>
                 </div>
             </section> 
-            <h1></h1>
-            <section className="section" id="card-section">
-            </section>                                                            
+            <h1 className="text-center title-matiere" style={{color: "#ed563b"}}>Filières et matiéres</h1>
+            <section className="section matiere_generale mb-4" id="card-section1">
+                <h1 className="title-generale mb-4">Matière du générale</h1>
+                <div className="container-fluid">
+                    <div className="row">
+                        <div className="col-md-12">
+                            <Carousel
+                            responsive={responsive}
+                        >
+                            <div>
+                                <BasicCard/>
+                            </div>
+                            <div>
+                                <BasicCard/>
+                            </div>
+                            <div>
+                                <BasicCard/>
+                            </div>
+                            <div>
+                                <BasicCard/>
+                            </div>
+                            <div>
+                                <BasicCard/>
+                            </div>
+                            </Carousel>
+                        </div>
+                    </div>
+                </div>
+            </section>      
+            <section className="section matiere_technique mb-4" id="card-section2">
+                <h1 className="title-technique mb-4">Matière du générale</h1>
+                <div className="container-fluid">
+                    <div className="row">
+                        <div className="col-md-12">
+                            <Carousel
+                            responsive={responsive}
+                        >
+                            <div>
+                                <BasicCard/>
+                            </div>
+                            <div>
+                                <BasicCard/>
+                            </div>
+                            <div>
+                                <BasicCard/>
+                            </div>
+                            <div>
+                                <BasicCard/>
+                            </div>
+                            <div>
+                                <BasicCard/>
+                            </div>
+                            </Carousel>
+                        </div>
+                    </div>
+                </div>
+            </section> 
+            <Footer/>                                                           
         </div>
     )
 }
--- src/views/home/card/Card.tsx
+++ src/views/home/card/Card.tsx
@@ -1,17 +1,19 @@
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
+import image from "../../../assets/images/image01.jpg"
+import './Card.css'
 
 function BasicCard() {
   return (
     <Card style={{ width: '18rem' }}>
-      <Card.Img variant="top" src="holder.js/100px180" />
+      <Card.Img variant="top" src={image} />
       <Card.Body>
         <Card.Title>Card Title</Card.Title>
         <Card.Text>
           Some quick example text to build on the card title and make up the
           bulk of the card's content.
         </Card.Text>
-        <Button variant="primary">Go somewhere</Button>
+        <Button className="button_header">Go somewhere</Button>
       </Card.Body>
     </Card>
   );
